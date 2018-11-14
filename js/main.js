3112f2f81966caa3fff0e0fbd649907cb0a2e1e4
Marion <marion.lotito@gmail.com>
2018-11-14 23:31:46 +0100

    added scrollify

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+$.scrollify({
+  section: ".scrollable-section",
+  sectionName: "section-name",
+  interstitialSection: "",
+  easing: "easeOutExpo",
+  scrollSpeed: 1100,
+  offset: 0,
+  scrollbars: true,
+  standardScrollElements: "",
+  setHeights: true,
+  overflowScroll: true,
+  updateHash: true,
+  touchScroll: true,
+  before: function() {},
+  after: function() {},
+  afterResize: function() {},
+  afterRender: function() {}
+});
