676b6a6bbd3ee315d955e0496ba81e1581b2dace
Marion <marion.lotito@gmail.com>
2018-12-09 13:59:38 +0100

    Started added modal for 1st project

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-$.scrollify({
-  section: ".scrollable-section",
-  sectionName: "section-name",
-  interstitialSection: "",
-  easing: "easeOutExpo",
-  scrollSpeed: 1100,
-  offset: 0,
-  scrollbars: true,
-  standardScrollElements: "",
-  setHeights: true,
-  overflowScroll: true,
-  updateHash: true,
-  touchScroll: true,
-  before: function() {},
-  after: function() {},
-  afterResize: function() {},
-  afterRender: function() {}
-});
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
