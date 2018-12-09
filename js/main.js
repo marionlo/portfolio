8ede1cf18b3e82ebc9529147b4b2b2b966c0deb2
Marion <marion.lotito@gmail.com>
2018-12-09 17:32:18 +0100

    content and style modal first project

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close = document.getElementsByClassName("fa-times")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
@@ -13,7 +13,7 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close.onclick = function() {
     modal.style.display = "none";
 }
 
