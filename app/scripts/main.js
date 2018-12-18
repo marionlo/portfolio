559f8f4e0c993a9d23df6cef20c931180d9a401b
Marion-Lotito <marion.lotito@gmail.com>
2018-12-18 23:53:17 +0100

    main fix on typed script and correct syntax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,3 @@
-// Typed script for the message on the first section
-var typed = new Typed('#typed', {
-  strings: ['Hi! I am Marion, a <span id="typed-front"><strong>Front-End Developer</strong></span> from France!'],
-  typeSpeed: 30,
-  backSpeed: 100,
-  fadeOut: true,
-
-});
-
 // Scroll animation
 $(function() {
   $.scrollify({
