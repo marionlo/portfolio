fd7ccc1c946e6cc2fef45a23eb7e9b5a6db44894
Marion-Lotito <marion.lotito@gmail.com>
2018-12-18 20:42:04 +0100

    add 5th project and footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,7 +10,7 @@ var typed = new Typed('#typed', {
 // Scroll animation
 $(function() {
   $.scrollify({
-    section: ".scrollable-section",
+    section: '.scrollable-section',
   });
 });
 
@@ -22,24 +22,24 @@ $(function() {
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById('myBtn');
 
 // Get the <span> element that closes the modal
-var close = document.getElementsByClassName("fa-times")[0];
+var close = document.getElementsByClassName('fa-times')[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
+    modal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
 close.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
 }
